6cc8f31a19de14a838649c129b52534e79a0c2fc
abhaik2001 <abhaimadhukaruvakkal@gmail.com>
2024-06-01 19:57:22 +0530

    new-code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import Login from "./components/Login.jsx";
+import { useStateProvider } from "./utils/StateProvider.jsx";
+import { reducerCases } from "./utils/Constnts.js";
+import Spotify from "./components/Spotify.jsx";
+
+export default function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [dispatch, token]);
+  return <div>{token ? <Spotify /> : <Login />}</div>;
+}
